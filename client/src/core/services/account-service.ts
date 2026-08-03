58eabe8e88cc48034cfe693e206bb6017fc5570f
Jessica Franke <jessicafranke715@gmail.com>
2026-08-03 15:32:51 -0500

    End of section 9

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Service, signal } from '@angular/core';
 import { LoginCreds, RegisterCreds, User } from '../../types/user';
 import { tap } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Service()
 export class AccountService {
   private http = inject(HttpClient);
   currentUser = signal<User | null>(null);
 
-  baseUrl = 'https://localhost:5001/api/';
+  private baseUrl = environment.apiUrl;
 
   register(creds: RegisterCreds) {
     return this.http.post<User>(this.baseUrl + 'account/register', creds).pipe(
