41b3258de63260f97d0f2fff3c67281cd38ae519
Jessica Franke <jessicafranke715@gmail.com>
2026-08-04 14:51:54 -0500

    End of section 10

--- API/Interfaces/IMemberRepository.cs
+++ API/Interfaces/IMemberRepository.cs
@@ -13,4 +13,6 @@ public interface IMemberRepository
     Task<Member?> GetMemberByIdAsync(string id);
 
     Task<IReadOnlyList<Photo>> GetPhotosForMemberAsync(string memberId);
+
+    Task<Member?> GetMemberForUpdateAsync(string id);
 }
--- client/src/core/services/member-service.ts
+++ client/src/core/services/member-service.ts
@@ -1,22 +1,33 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Service } from '@angular/core';
+import { inject, Service, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Member, Photo } from '../../types/member';
+import { EditableMember, Member, Photo } from '../../types/member';
+import { tap } from 'rxjs';
 
 @Service()
 export class MemberService {
   private http = inject(HttpClient);
   private baseUrl = environment.apiUrl;
+  editMode = signal(false);
+  member = signal<Member | null>(null);
 
   getMembers() {
     return this.http.get<Member[]>(this.baseUrl + 'members');
   }
 
   getMember(id: string) {
-    return this.http.get<Member>(this.baseUrl + 'members/' + id);
+    return this.http.get<Member>(this.baseUrl + 'members/' + id).pipe(
+      tap((member) => {
+        this.member.set(member);
+      }),
+    );
   }
 
   getMemberPhotos(id: string) {
     return this.http.get<Photo[]>(this.baseUrl + 'members/' + id + '/photos');
+  }
+
+  updateMmeber(member: EditableMember) {
+    return this.http.put(this.baseUrl + 'members', member);
   }
 }
